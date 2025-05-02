3798fa63a6c4420b74d20579b37c3067c337b0b2
ChandanaRondla <chandanareddy235@gmail.com>
2025-05-02 13:52:51 -0700

    Added root route to display backend live message

--- backend/health_monitoring/urls.py
+++ backend/health_monitoring/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("✅ Django backend is running!")
 
 urlpatterns = [
+    path('', home_view),  # ✅ Root route
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),              # ✅ Your app’s URLs
-    path('accounts/', include('django.contrib.auth.urls')),
-    path("api/", include("predictor.urls")),# ✅ Built-in login/logout
+    path('accounts/', include('accounts.urls')),              
+    path('accounts/', include('django.contrib.auth.urls')),   # Built-in login/logout
+    path("api/", include("predictor.urls")),
 ]
